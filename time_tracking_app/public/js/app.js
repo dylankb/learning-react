9b7d51ca86b5c025cb12dcb3df4e69b78e426628
dylankb <dylan.barnard@gmail.com>
2018-02-05 22:30:33 -0800

    Save state on server using client library

--- time_tracking_app/public/js/app.js
+++ time_tracking_app/public/js/app.js
@@ -1,21 +1,18 @@
 class TimersDashboard extends React.Component {
   state = {
-    timers: [
-      {
-        title: 'Practice squat',
-        project: 'Gym Chores',
-        id: uuid.v4(),
-        elapsed: 5456099,
-        runningSince: Date.now(),
-      }, {
-        title: 'Bake squash',
-        project: 'Kitchen Chores',
-        id: uuid.v4(),
-        elapsed: 1273998,
-        runningSince: null,
-      },
-    ],
+    timers: [],
   };
+
+  componentDidMount() {
+    this.loadTimersFromServer();
+    setInterval(this.loadTimersFromServer, 5000)
+  }
+
+  loadTimersFromServer = () => {
+    client.getTimers((serverTimers) => {
+      this.setState({ timers: serverTimers })
+    });
+  }
 
   handleStartClick = (timerId) => {
     this.startTimer(timerId);
@@ -35,6 +32,10 @@ class TimersDashboard extends React.Component {
         }
       }),
     });
+
+    client.startTimer(
+      { id: timerId, start: now }
+    );
   };
 
   handleStopClick = (timerId) => {
@@ -57,6 +58,10 @@ class TimersDashboard extends React.Component {
         }
       }),
     });
+
+    client.stopTimer(
+      { id: timerId, stop: now }
+    );
   };
 
   handleTrashClick = (timerId) => {
@@ -67,6 +72,10 @@ class TimersDashboard extends React.Component {
     this.setState({
       timers: this.state.timers.filter(timer => timer.id != timerId)
     });
+
+    client.deleteTimer(
+      { id: timerId }
+    );
   };
 
   handleEditFormSubmit = (attrs) => {
@@ -82,6 +91,8 @@ class TimersDashboard extends React.Component {
     this.setState({
       timers: this.state.timers.concat(t),
     });
+
+    client.createTimer(t);
   };
 
   updateTimer = (attrs) => {
@@ -97,6 +108,8 @@ class TimersDashboard extends React.Component {
         }
       }),
     });
+
+    client.updateTimer(attrs);
   };
 
   render() {
