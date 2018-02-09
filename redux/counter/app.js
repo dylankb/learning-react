35e3f5c93c8fc26e451e65629822bbb134a182d2
dylankb <dylan.barnard@gmail.com>
2018-02-08 17:03:48 -0800

    Create basic chat store and reducer

--- redux/counter/app.js
+++ redux/counter/app.js
@@ -8,8 +8,30 @@ function reducer(state, action) {
   }
 }
 
-const incrementAction = { type: 'INCREMENT', amount: 1 };
-const decrementAction = { type: 'DECREMENT', amount: 1 };
+function createStore(reducer) {
+  let state = 0;
+  const getState = () => (state);
 
-console.log(reducer(0, incrementAction));
-console.log(reducer(10, decrementAction));
+  const dispatch = (action) => {
+    state = reducer(state, action);
+  }
+
+  return {
+    getState,
+    dispatch,
+  }
+}
+
+const store = createStore(reducer);
+
+const incrementAction = { type: 'INCREMENT', amount: 3 };
+
+store.dispatch(incrementAction);
+console.log(store.getState()); // -> 3
+store.dispatch(incrementAction);
+console.log(store.getState()); // -> 6
+
+const decrementAction = { type: 'DECREMENT', amount: 4 };
+
+store.dispatch(decrementAction);
+console.log(store.getState()); // -> 2
