ba65e339db3008eb641b22cdf0bbb9bd1383d33a
dylankb <dylan.barnard@gmail.com>
2018-02-08 19:55:38 -0800

    Add listener functionality
    Uses pub-sub to fire callbacks for components on store state changes

--- redux/chat_simple/src/App.js
+++ redux/chat_simple/src/App.js
@@ -1,13 +1,20 @@
 function createStore(reducer, initialState) {
   let state = initialState;
+  const listeners = [];
+
+  const subscribe = (listener) => (
+    listeners.push(listener);
+  );
 
   const getState = () => (state);
 
   const dispatch = (action) => {
     state = reducer(state, action);
+    listeners.forEach(l => l()); // invoke all funcs in listeners on state change
   };
 
   return {
+    subscribe,
     getState,
     dispatch,
   };
@@ -35,32 +42,27 @@ function reducer(state, action) {
 const initialState = { messages: [] };
 const store = createStore(reducer, initialState);
 
+const listener = () => {
+  console.log('Current state: ');
+  console.log(store.getState());
+}
+
+store.subscribe(listener);
+
 const addMessageAction1 = {
   type: 'ADD_MESSAGE',
-  message: 'How does it look, Neil?',
+  message: 'How do you read?',
 };
+store.dispatch(addMessageAction1); // listener() is called
 
-store.dispatch(addMessageAction1);
-const stateV1 = store.getState();
-
-const addMessageAction2 = { type: 'ADD_MESSAGE',
-                            message: 'Looking good.',
-                          };
-
-store.dispatch(addMessageAction2);
-const stateV2 = store.getState();
-
-console.log('State v1:');
-console.log(stateV1);
-console.log('State v2:');
-console.log(stateV2);
+const addMessageAction2 = {
+  type: 'ADD_MESSAGE',
+  message: 'I read you loud and clear, Houston.',
+};
+store.dispatch(addMessageAction2); // listener() is called
 
 const deleteMessageAction = {
   type: 'DELETE_MESSAGE',
   index: 0,
 };
-
-store.dispatch(deleteMessageAction);
-const stateV3 = store.getState();
-console.log('State v3:');
-console.log(stateV3);
+store.dispatch(deleteMessageAction); // ...
