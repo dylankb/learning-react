546b028a9b14d683352b39b8156250d0831826f8
dylankb <dylan.barnard@gmail.com>
2018-02-19 19:16:36 -0800

    Wire up close button to deselect album

--- routing/music/client/src/components/AlbumsContainer.js
+++ routing/music/client/src/components/AlbumsContainer.js
@@ -63,6 +63,7 @@ class AlbumsContainer extends Component {
                 return (
                   <Album
                     album={album}
+                    albumsPathname={matchPath}
                   />
                 );
               }}
